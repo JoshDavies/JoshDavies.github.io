974c2bffa44676218768c2570ae1fdf459117969
Josh Davies <jdaviescreative@gmail.com>
2020-01-15 16:56:03 +0000

    Updates

---
+++ precache-manifest.898e2b4322937b8bf2379b42b78eec9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e2b4dad8ac6bfa78197703f541d6a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "404f1d3191f3ec8b57fa",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8d194ce03e5d42947352",
+    "url": "/static/js/2.201d0598.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.201d0598.chunk.js.LICENSE"
+  },
+  {
+    "revision": "404f1d3191f3ec8b57fa",
+    "url": "/static/js/main.5157cd41.chunk.js"
+  },
+  {
+    "revision": "0f893987d35ede8d36f7",
+    "url": "/static/js/runtime-main.6eab9f04.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
